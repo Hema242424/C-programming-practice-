d3494d8c7264ceb907820d27079c5dd224ae1052
Hema Maurya <97778541+Hema242424@users.noreply.github.com>
2024-07-24 04:59:26 +0530

    Update Loop 03 SearchNumFromGivenNum.c

--- 01 Syntax of C Programming/Loop 03 SearchNumFromGivenNum.c
+++ 01 Syntax of C Programming/Loop 03 SearchNumFromGivenNum.c
@@ -1,1 +1,19 @@
-#include <stdio.h> int main() { int n, s=0; printf("give any no\n"); scanf("%d", &n); printf("--start searching ----\n"); for(int i=1;i<=50;i++){ if (i ==n){ s=1; break; } } if(s==1){ printf("num found"); } else { printf("num not found"); } return 0; }
+#include <stdio.h> 
+int main() { 
+  int n, s=0; printf("give any no\n"); 
+  scanf("%d", &n); 
+  
+  printf("--start searching ----\n"); 
+  for(int i=1;i<=50;i++){ 
+    if (i ==n){ 
+      s=1; 
+      break; 
+    } 
+  } 
+  if(s==1){ 
+    printf("num found"); 
+  } else { 
+    printf("num not found"); 
+  } 
+  return 0; 
+}
