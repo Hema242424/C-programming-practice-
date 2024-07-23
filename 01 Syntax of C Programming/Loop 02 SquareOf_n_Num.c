819d52d86327c70288976de67e9ebfe7dce1d5d1
Hema Maurya <97778541+Hema242424@users.noreply.github.com>
2024-07-24 04:32:16 +0530

    Update Loop 02 SquareOf_n_Num.c

--- 01 Syntax of C Programming/Loop 02 SquareOf_n_Num.c
+++ 01 Syntax of C Programming/Loop 02 SquareOf_n_Num.c
@@ -1,1 +1,1 @@
-
+#include <stdio.h> int main() { int n; printf("give any no\n"); scanf("%d", &n); printf("----printf square of numbers---\n"); for(int i=1;i<=n;i++){ printf("square of %d = %d \n", i, i*i); } return 0; }
