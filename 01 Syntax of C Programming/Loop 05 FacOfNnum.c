01ee83f15626ad936fd3b4d5395b87ed0a333b74
Hema Maurya <97778541+Hema242424@users.noreply.github.com>
2024-07-24 19:14:55 +0530

    Update Loop 05 FacOfNnum.c

--- 01 Syntax of C Programming/Loop 05 FacOfNnum.c
+++ 01 Syntax of C Programming/Loop 05 FacOfNnum.c
@@ -1,1 +1,20 @@
 
+#include<stdio.h>
+int main(){
+int n, fac=1;
+printf ("enter number for factorial\n");
+scanf("%d", &n);
+
+if(n==0 && n==1){ 
+fac=1;
+}
+else{
+int i=2;
+while(i<=n){
+    fac=fac*i;
+    i++;
+}
+printf("fac= %d",fac );
+
+}
+}
